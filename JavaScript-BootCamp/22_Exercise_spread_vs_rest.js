fbb3aaa2ea0b8bb2279d38f72d3147020fb3e76e
jishan101 <rko.jishan@gmail.com>
2021-10-08 02:47:34 +0600

    update

--- JavaScript-BootCamp/22_Exercise_spread_vs_rest.js
+++ JavaScript-BootCamp/22_Exercise_spread_vs_rest.js
@@ -5,7 +5,7 @@ function countTruthy(arr) {
     let count = 0;
 
     for(let i = 0; i < arr.length; i++) {
-        if(!!arr[i]) {
+        if(arr[i]) {
             count++;
             console.log(arr[i]);
         }
